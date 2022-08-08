8517257fbc0646b305ec2ae42522d1731c155e86
Bilal Abbas <babbas0505@gmail.com>
2022-08-09 00:26:22 +0100

    passing initial specs

--- models/express-models/todos.js
+++ models/express-models/todos.js
@@ -18,14 +18,22 @@ module.exports = {
   // ==== COMPLETE THE FOLLOWING (SEE `model.js` TEST SPEC) =====
   listPeople: function () {
     // returns an array of all people for whom tasks exist
+    let returnArray = Object.keys(tasks);
+    return returnArray;
   },
 
   add: function (name, task) {
     // saves a task for a given person
+    if (tasks[name]) {
+      tasks[name].push(task)
+    } else {
+      tasks[name] = [task]
+    }
   },
 
   list: function (name) {
     // returns tasks for specified person
+    return tasks[name];
   },
 
   complete: function (name, idx) {
